cf25c07eea6ee292733bc865e495bc95f38f17b3
itsdiy0 <diyo11jen@gmail.com>
2025-05-27 14:32:33 +0100

    initialsed browser extention

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import relay from 'vite-plugin-relay'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
   plugins: [
     react(),
     relay,
+    tailwindcss()
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
